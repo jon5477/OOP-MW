63f425297ea3a80494cbf173f849c52a38e52fe4
zhya215 <zhya1033@colorado.edu>
2015-03-29 15:29:47 -0700

    changed in routing and controller for searching cities

---
+++ app/Http/Controllers/CountryController.php
@@ -0,0 +1,22 @@
+<?php namespace App\Http\Controllers;
+
+class CountryController extends Controller {
+
+	/*
+	|--------------------------------------------------------------------------
+	| Home Controller
+	|--------------------------------------------------------------------------
+	|
+	| This controller renders your application's "dashboard" for users that
+	| are authenticated. Of course, you are free to change or remove the
+	| controller as you wish. It is just here to get your app started!
+	|
+	*/
+
+
+	public function searchByName()
+	{
+		return 'searchByName';
+	}
+
+}
