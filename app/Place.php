7b7ad492d846fc00377944ceb54f5df32e71279c
jon5477 <huangjonathan@live.com>
2015-03-30 21:23:04 -0600

    Added Place and Country

--- app/Place.php
+++ app/Place.php
@@ -2,41 +2,33 @@
 
 use Illuminate\Database\Eloquent\Model;
 
-class Location extends Model {
-
-
-
-
+abstract class Place extends Model {
 	/**
 	 * The attributes that are mass assignable.
 	 *
 	 * @var array
 	 */
-	protected $fillable = ['longitued', 'latitude', 'population', 'name'];
+	protected $guarded = ['id', 'longitude', 'latitude', 'population', 'name'];
 
 	public function getId(){
-
+		return $this->id;
 	}
 
 	public function getLongitude(){
-
+		return $this->longitude;
 	}
 
 	public function getLatitude(){
-
+		return $this->latitude;
 	}
 
 	public function getPopulation(){
-
+		return $this->population;
 	}
 
 	public function getName(){
-
+		return $this->name;
 	}
 
-	public function returnData(){
-		
-	}
-
-
+	public abstract function returnData();
 }
