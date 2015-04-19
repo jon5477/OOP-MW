0c2f8d880311bfc0817483cece4af325d81037ef
lota0592 <lota0592@colorado.edu>
2015-04-19 15:53:57 -0600

    models submit

--- app/City.php
+++ app/City.php
@@ -2,26 +2,8 @@
 
 use Illuminate\Database\Eloquent\Model;
 
-class City extends Place {
+class City extends Model {
 
-
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
-	protected $table = 'cities';
-
-	/**
-	 * The attributes that are mass assignable.
-	 *
-	 * @var array
-	 */
-	array_push(parrent::$fillable, 'cityId');
-
-
-	public function returnData(){
-		return this->data;
-	}
+	//
 
 }
