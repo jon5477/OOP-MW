6ee2d93d79afc2f101642358e6203e592fab2b69
zhya215 <zhya1033@colorado.edu>
2015-04-20 19:14:16 -0700

    fixed userpage

--- resources/views/userpage.blade.php
+++ resources/views/userpage.blade.php
@@ -6,22 +6,28 @@
 	<p>welcome {!! $userid !!}</p>
 	
 </head>
-<div id="main">
-	<div id="countrydata">
-		<select name="country" size="20">
-		@foreach ($data as $d)
-		<option value="{{ $d }}" onclick="fetchCountryData(this.value)">{{ $d	 }}</option>
-		@endforeach
-		</select>
-	</div>
-	<div id="citydata">
-		<select name="city" size="20">
-		@foreach ($data1 as $d1)
-		<option value="{{ $d1 }}" onclick="fetchCityData(this.value)">{{ $d1	 }}</option>
-		@endforeach
-		</select>
+<table>
+	<tr>
+		<th>
+			<div id="countrydata">
+				<select name="country" size="20">
+				@foreach ($data as $d)
+				<option value="{{ $d }}" onclick="fetchCountryData(this.value)">{{ $d	 }}</option>
+				@endforeach
+				</select>
+			</div>
+		</th>
+		<th>
+			<div id="citydata">
+				<select name="city" size="20">
+				@foreach ($data1 as $d1)
+				<option value="{{ $d1 }}" onclick="fetchCityData(this.value)">{{ $d1	 }}</option>
+				@endforeach
+				</select>
 
-	</div>
-</div>
+			</div>
+		</th>
+	</tr>
+</table>
 
 @stop
